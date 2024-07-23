faed29d017992697805da58a0eea51d2e571cefb
Mona Lisa <dev@yadea.com.cn>
2024-07-24 00:55:04 +0800

    chore: 练习32题

--- exercises/06_move_semantics/move_semantics2.rs
+++ exercises/06_move_semantics/move_semantics2.rs
@@ -14,15 +14,15 @@ fn main() {
 mod tests {
     use super::*;
 
-    // TODO: Make both vectors `vec0` and `vec1` accessible at the same time to
     // fix the compiler error in the test.
     #[test]
     fn move_semantics2() {
         let vec0 = vec![22, 44, 66];
 
+        assert_eq!(vec0, [22, 44, 66]);
+
         let vec1 = fill_vec(vec0);
 
-        assert_eq!(vec0, [22, 44, 66]);
         assert_eq!(vec1, [22, 44, 66, 88]);
     }
 }
